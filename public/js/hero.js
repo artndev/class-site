e5accd04546290719c3f3577d0d07bd091e5e5ce
artndev <97910780+artndev@users.noreply.github.com>
2023-03-07 20:44:10 +0300

    Small fixes

--- public/js/hero.js
+++ public/js/hero.js
@@ -1,6 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    gsap.timeline({stagger: 0.1})
-        .fromTo(".hero__logo", {
+    // ? Arrow
+    const arrow = document.querySelector( ".arrow" );
+
+    // ? Hero nodes
+    const hero = document.querySelector( "#hero" );
+    const hero_logo = document.querySelector( ".hero__logo" );
+    const hero_desc = document.querySelector( ".hero__desc" );
+
+    // ? Burger && lines of burger
+    const burger = document.querySelector( ".header__menu-burger" );
+    const lines = Array.from( document.querySelectorAll(".line") );
+
+
+    gsap.timeline({ stagger: 0.1 })
+        .fromTo(hero_logo, {
             duration: 1,
             ease: "power1.inOut",
             transform: "translateX(-100%)",
@@ -11,7 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
             transform: "translateX(0%)",
             opacity: 1
         })
-        .fromTo(".hero__desc", {
+        .fromTo(hero_desc, {
             duration: 1,
             ease: "power1.inOut",
             transform: "translateY(-100%)",
@@ -22,7 +35,7 @@ document.addEventListener("DOMContentLoaded", () => {
             transform: "translateX(0%)",
             opacity: 1
         })
-        .fromTo(".arrow", {
+        .fromTo(arrow, {
             duration: 1,
             ease: "power1.inOut",
             transform: "translateY(-100%)",
@@ -30,24 +43,33 @@ document.addEventListener("DOMContentLoaded", () => {
         }, {
             duration: 1,
             ease: "power1.inOut",
-            transform: "translateX(0%)",
+            transform: "translateY(0%)",
             opacity: 1
-        })
-    gsap.timeline({yoyo: true, repeat: -1}) 
-        .fromTo(".arrow", {
-            duration: 1,
-            ease: "power1.inOut",
-            y: "-10"
-        }, {
-            duration: 1,
-            ease: "power1.inOut",
-            y: "10"
-        })
-})
+        });
 
-$(".arrow").click(() => {
-    gsap.to(window, {
-        duration: 1,
-        scrollTo: "#about__section"
-    })
-})
+        gsap.timeline({ yoyo: true, repeat: -1 }) 
+            .fromTo(arrow, {
+                duration: 1,
+                ease: "power1.inOut",
+                y: "-10"
+            }, {
+                duration: 1,
+                ease: "power1.inOut",
+                y: "10"
+            });
+
+            
+        arrow.addEventListener("click", () => {
+            gsap.to(window, {
+                duration: 0.5,
+                scrollTo: screen.availWidth <= 460
+                    ? { y: "#about__section" }
+                    : { y: "#about__section", offsetY: 100 },
+                onComplete: function () {
+                    if (Object.keys( this.vars.scrollTo ).length === 1) {
+                        closeHeader();
+                    }
+                }
+            });
+        });
+});
